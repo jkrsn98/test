048b5685d5735b5e2f66458540d8ba5081a7c194
jkrsn98 <justinkrasner98@gmail.com>
2020-04-18 06:18:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ $.ajax({
     console.log(data);
     temp= Math.floor(data.currently.temperature);
     windy = Math.floor(data.currently.windSpeed)>9?true:false;
-    rain = (data.currently.precipProbability)>0.5?true:false;
+    rain = (data.currently.precipProbability)>.5?true:false;
     cloudy = (data.currently.cloudCover)>0.45?true:false;
     console.log("windy?" + windy);
     console.log("cloudy?" + cloudy);
